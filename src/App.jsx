c8028656ab75d3368ff0b88caad5e6b8d66b16f4
DESKTOP-FGDRREL\ellio <elliotcase@hotmail.co.uk>
2023-11-18 14:52:22 +0000

    added top button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import TopButtons from "./components/TopButtons";
 
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold text-red-700">hello</h1>
+      <div
+        className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit
+      shadow-xl shadow-gray-400"
+      >
+        <TopButtons />
+      </div>
     </>
   );
 }
